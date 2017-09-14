ff9aa26ecabe8dc37f18abf84768ee10500b11f8
tangjun <tangjun@tenda.cn>
2017-09-14 08:57:54 +0800

    Initial Commit

---
+++ Parrot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Parrot
+//
+//  Created by tenda_dev on 2017/9/14.
+//  Copyright © 2017年 tomjoy1947. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Parrot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Parrot
+//
+//  Created by tenda_dev on 2017/9/14.
+//  Copyright © 2017年 tomjoy1947. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
